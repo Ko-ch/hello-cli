22d0ec9dd9bfa19f018836c2bca0bde999e06540
Koichiro <koichiro_takagi@outlook.jp>
2019-09-04 22:22:47 +0900

    とりま変更

---
+++ app.py
@@ -0,0 +1,12 @@
+print("What's your name? > ")
+yourname = input()
+print('Hi! Fucking {}'.format(yourname))
+print(f"Hi! Fucking {yourname}")
+print("Hi! Fucking " + yourname)
+print("Hi! Fucking ")
+
+print('koichiro' + str(1) + yourname)
+
+
+
+breakpoint()
